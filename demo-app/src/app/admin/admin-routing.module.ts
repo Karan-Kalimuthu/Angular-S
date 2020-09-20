305149bb4954a598022cbbedc8471e93621b88d4
Karan Kalimuthu <56226319+Karan-Kalimuthu@users.noreply.github.com>
2020-09-20 17:56:00 +0530

    Using Guards

--- demo-app/src/app/admin/admin-routing.module.ts
+++ demo-app/src/app/admin/admin-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminDashComponent } from './admin-dash/admin-dash.component';
+import { AdminOnlyGuard } from './admin-only.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: AdminDashComponent
+    component: AdminDashComponent,
+    canActivate: [AdminOnlyGuard]
   },
   {
     path: 'dashboard',
